24887ecf9a4a72d7739ce2b20cef0a5ebedf173a
Basti <2233205@slu.edu.ph>
2023-03-09 22:58:57 +0800

    Update file MixedFraction.java

--- src/prog2/midgroup01/MixedFraction.java
+++ src/prog2/midgroup01/MixedFraction.java
@@ -38,6 +38,8 @@ public class MixedFraction extends Fraction {
 
     /**
      * Constructor that is receiving a given fraction.
+     *
+     * testiestjesitnesitesntiesntiesnt
      * @param fraction
      */
     public MixedFraction(Fraction fraction) {
