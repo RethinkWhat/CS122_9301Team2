8fef1cbbdf4e8a4f9f5008cb844510d85d3274b9
Rithik Tank <2233293@slu.edu.ph>
2023-03-15 17:05:26 +0800

    Minor adjustments to algorithms

--- src/prog2/midgroup01/FractionTester.java
+++ src/prog2/midgroup01/FractionTester.java
@@ -17,22 +17,9 @@
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
 
  Problem Description:
- 1. Create a project in IntelliJ IDEA with the following naming convention:
- <last names of the members (in alphabetical order)> <class code>PrelimGroupProject
- e.g. AranetaAyalaMontelibanoTan9300PrelimGroupProject
- 2. Create a package within the project named “prog2.prelimgroup”. All classes needed for this activity
- should be placed within this package.
- 3. Create a class called Fraction. The Fraction class should define a “new data type” such that it will be
- used as the data type for a variable that will hold a typical fraction. A typical fraction is composed of a
- numerator and a denominator.
- 4. Create a class called FractionArithmetic. The FractionArithmetic class should be an executable
- program (class having the main method) that will allow the user to compute the sum, difference, product
- and quotient of two fractions. When FractionArithmetic is executed, the user will be asked to enter the
- values of the numerator and denominator of both fractions and then a menu will be shown from which
- he/she can choose to enter value for fraction 1, enter value for fraction 2, add, subtract, multiply, divide
- the fractions, reduce a fraction, and quit from the program. The result (sum, difference, product, quotient,
- or reduce a fraction) should be displayed in the typical fraction form and its equivalent decimal value.
-
+    Create a program that facilitates addition, subtraction, multiplication and division of mixed
+ numbers (similar with the first activity) but this time all of the inputs/outputs may involve fractions, mixed
+ fractions or combination.
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
 
  Requirement:
@@ -49,103 +36,13 @@
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
 
  Inputs:
- integer number for menu choice
- numerator of a fraction
- denominator of a fraction
 
  Outputs:
- sum of two fractions
- difference of two fractions
- product of two fractions
- quotient of two fractions
- fraction in its lowest terms
 
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
- UML Diagram:
- +----------------------------------------------------------------------+
- |                          FractionArithmetic                          |
- +----------------------------------------------------------------------+
- -keyboard: int                                                         |
- -input1Exists: boolean                                                 |
- -input2Exists: boolean                                                 |
- +----------------------------------------------------------------------+
- | + run(): void                                                        |
- | +showIntroduction(): void                                            |
- | +showChoices(): void                                                 |
- | +enterPick(min: int, max: int): int                                  |
- | +requestFraction()                                                   |
- | +requestFraction1()                                                  |
- | +requestFraction2()                                                  |
- | +addition(fraction1: Fraction, otherFraction: Fraction): void        |
- | +subtraction(fraction1: Fraction, otherFraction: Fraction): void     |
- | +multiplication(fraction1: Fraction, otherFraction: Fraction): void  |
- | +division(fraction1: Fraction, otherFraction: Fraction): void        |
- | +reduce(fractionToBeReduced: Fraction): void                         |
- | +presskey(): void                                                    |
- | +showExit(): void                                                    |
- +----------------------------------------------------------------------+
+
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
  Algorithm:
- 1. Print an introduction statement that displays information about the purpose
- of the program and guidelines for the user.
- 2. Implement a do-while statement that displays the available program choices
- to the user for them to pick from.
- 3. If the user input for the value of the first fraction and second fraction does not exist. Only print
- limited program choices in a main menu where the user can only choose to input the value of the first
- and second fraction or exit the program before proceeding to other available program choices.
- 4. Read the integer through user input that corresponds to the user's choice.
- 5. Implement a switch-case statement that provides cases for a specific routine.
- 6. If integer equal to 1 is entered, the program will proceed to the menu choice number 1 and does the following:
- - Read an integer through keyboard input from the user for the numerator of the first fraction. Given an
- appropriate statement for invalid inputs.
- - Read an integer through keyboard input from the user for the denominator of the first fraction. The integer
- should not be equal to 0 to avoid an undefined result. Given an appropriate statement for invalid inputs.
- - Convert the decimal equivalent of the fraction.
- - Print the fraction and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 7. If integer equal to 2 is entered, the program will proceed to the menu choice number 2 and does the following:
- - Read an integer through keyboard input from the user for the numerator of the second fraction. Given an
- appropriate statement for invalid inputs.
- - Read an integer through keyboard input from the user for the denominator of the second fraction. The integer
- should not be equal to 0 to avoid an undefined result. Given an appropriate statement for invalid inputs.
- - Convert the decimal equivalent of the fraction.
- - Print the fraction and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 8. If the user input for the value of the first fraction and second fraction does exist. Print
- all the available program choices shown in a main menu.
- 9. Read the integer through user input that corresponds to the user's choice.
- 10. If integer equal to 3 is entered, the program will proceed to the menu choice number 3 and does the following:
- - Add the value of the first fraction and second fraction.
- - Convert the decimal equivalent of the sum of the two fractions.
- - Print the sum value and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 11. If integer equal to 4 is entered, the program will proceed to the menu choice number 4 and does the following:
- - Subtract the value of the first fraction and second fraction.
- - Convert the decimal equivalent of the difference of the two fractions.
- - Print the difference value and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 12. If integer equal to 5 is entered, the program will proceed to the menu choice number 5 and does the following:
- - Multiply the value of the first fraction and second fraction.
- - Convert the decimal equivalent of the product of the two fractions.
- - Print the product value and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 13. If integer equal to 6 is entered, the program will proceed to the menu choice number 6 and does the following:
- - Divide the value of the first fraction and second fraction.
- - Convert the decimal equivalent of the quotient of the two fractions.
- - Print the quotient value and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 14. If integer equal to 7 is entered, the program will proceed to the menu choice number 7 and does the following:
- - Read an integer through keyboard input from the user for the numerator of the fraction. Given an
- appropriate statement for invalid inputs.
- - Read an integer through keyboard input from the user for the denominator of the fraction. The integer
- should not be equal to 0 to avoid an undefined result. Given an appropriate statement for invalid inputs.
- - Reduce the value of the fraction to the lowest terms.
- - Convert the decimal equivalent of the lowest term value of the fraction.
- - Print the lowest term value and its decimal equivalent.
- - Let the user press the enter key to proceed back to the main menu of user choices.
- 15. If integer equal to 8 is entered, the program will proceed to the menu choice number 8 and does the following:
- - Display a closing statement of the program.
- - Exit the program.
 
  〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
  */
